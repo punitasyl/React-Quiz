67d374542aa8a248e782294ddf15d5a123cf2dee
Assylbek2021 <asylbekzhardekov@gmail.com>
2022-01-15 02:14:41 +0600

    Create App QUIZ

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+import classes from "./Quiz.module.scss";
+
+class Quiz extends Component {
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                <h1>Quiz</h1>
+            </div>
+        )
+    }
+}
+
+export default Quiz
